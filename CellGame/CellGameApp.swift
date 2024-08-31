5ed9d2aa4f0f1f41cadc56448ba6621b0efae4e8
artemgarshin <garshinareem@gmail.com>
2024-08-31 12:20:26 +0300

    Initial Commit

---
+++ CellGame/CellGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CellGameApp.swift
+//  CellGame
+//
+//  Created by Артем Гаршин on 31.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct CellGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
